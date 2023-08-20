556c529ea5b2659cafd1004efee760297a27c9e3
LeoSouza221 <leosouza221@gmail.com>
2023-08-20 19:20:06 -0300

    chore: improvements

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -3,9 +3,10 @@
   import { useRoute } from 'vue-router'
   import { useTodo } from '@/utils/composables/todoComposable'
   import ListTodoItems from '@/components/ListTodoItems.vue'
+  import IconBack from '@/components/icons/IconBack.vue'
 
   const isConclude = ref(false)
-  const { todoItems } = useTodo()
+  const { todoItems, concludeItems } = useTodo()
   const route = useRoute()
 
   onMounted(() => {
@@ -16,14 +17,20 @@
 </script>
 
 <template>
-  <div class="container-height dark:text-white">
+  <div class="py-3 dark:text-white">
+    <div class="py-3 md:absolute">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold p-1 rounded-full" @click="$router.go(-1)">
+        <IconBack />
+      </button>
+    </div>
     <div class="flex justify-center">
       <div class="card max-w-[600px] w-[600px] dark:bg-slate-700 dark:text-white">
         <h1 class="text-3xl font-bold py-2">Itens {{ isConclude ? 'concluídos' : 'pendentes' }}</h1>
         <div class="border-b">
         </div>
         <div class="flex">
-          <ListTodoItems v-model="todoItems" :is-conclude="isConclude" />
+          <ListTodoItems v-if="isConclude" v-model="concludeItems" :is-conclude="isConclude" />
+          <ListTodoItems v-else v-model="todoItems" />
         </div>
       </div>
     </div>
